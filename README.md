b7bf8513466f08a159b416c27d0b719393b3bc11
golangdevm <honest.dedicate@gmail.com>
2023-06-17 22:51:37 -0700

    Showing Alert Dialogs

--- README.md
+++ README.md
@@ -14,3 +14,18 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## Quick Run Project
+
+```
+git clone https://github.com/faithcredit/flutter_shop.git
+
+cd flutter_shop
+flutter run
+```
+![1](/docs/1.png)
+![2](/docs/2.png)
+![3](/docs/3.png)
+![4](/docs/4.png)
+![5](/docs/5.png)
+![6](/docs/6.png)
--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -37,6 +37,32 @@ class CartItem extends StatelessWidget {
         ),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+                title: const Text('Are you sure?'),
+                content: const Text(
+                  'Do you want to remove the item from the cart?',
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text('No'),
+                    onPressed: () {
+                      Navigator.of(ctx).pop(false);
+                    },
+                  ),
+                  TextButton(
+                    child: const Text('Yes'),
+                    onPressed: () {
+                      Navigator.of(ctx).pop(true);
+                    },
+                  ),
+                ],
+              ),
+        );
+      },
+      
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
